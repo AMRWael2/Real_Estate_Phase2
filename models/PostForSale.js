cfc3f3b21ad4a2747dfb5fca8150a6f896a8ef41
MariamNasr <152440928+MariamN-x@users.noreply.github.com>
2024-06-22 11:22:52 +0300

    Add files via upload

--- models/PostForSale.js
+++ models/PostForSale.js
@@ -4,8 +4,10 @@ const postForSaleSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    location: { type: String, required: true }
-});
+    location: { type: String, required: true },
+    imagePath:{type: String, required: true}
+}, { timestamps: true });
+
 
 const PostForSale = mongoose.model('PostForSale', postForSaleSchema);
 
